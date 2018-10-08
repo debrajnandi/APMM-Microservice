04865ca84f64e64e5192daaa431cc94564e87820
antamukh <antara.mukherjee83@gmail.com>
2018-10-08 11:31:45 +0530

    Removed Some Projects

--- PersonSpringBootJPAh2/src/main/java/com/example/demo/dao/PersonDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.model.Person;
-
-public interface PersonDAO extends CrudRepository<Person, Integer> {
-
-}
--- PersonSpringBootJPAh2/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into person values (1001, '35', 'Antara');
-insert into person values (1002, '37', 'Animesh');
-insert into person values (1003, '6', 'Adwitiya');
